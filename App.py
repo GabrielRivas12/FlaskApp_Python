2df1ced985bcdd488812f46e25f39f09d42aad30
GabrielRivas12 <gabrielmrivash@gmail.com>
2025-08-29 23:57:47 -0600

    adaptacion de diseño en inicio

--- App.py
+++ App.py
@@ -2,22 +2,25 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
-@app.route('/')  # Decorador de la ruta principal
+@app.route('/inicio')  # Decorador de la ruta principal
 def inicio():
     return render_template('index.html')
 
 @app.route('/contacto')
 def contacto(): # funtion de la ruta de contactos
-    return "Pagina de contactosss"
+    return render_template('Contactos.html')
 
 @app.route('/about') # funtion de la ruta de about
 def about():
-    return "esta es el acerca de"
+    return render_template('About.html')
 
-@app.route('/servicios/<nombre>')
-def servicios (nombre):
-    return 'El nomnre del servicio es %s ' % nombre
+@app.route('/login') # 
+def login():
+    return render_template('Login.html')
 
+@app.route('/Registro') # 
+def Registro():
+    return "render_template('Registro.html')" 
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000) #ejecuta la aplicacion en modo depuracion
