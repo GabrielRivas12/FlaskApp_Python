c411252f13a31e67a25f5bd4e0899ddfe7e1316e
GabrielRivas12 <gabrielmrivash@gmail.com>
2025-08-30 10:50:02 -0600

    Adapataciones de diseño para inicio, login y registrase

--- App.py
+++ App.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app=Flask(__name__)
 
+@app.route('/')  # Decorador de la ruta principal
+def home():
+    return render_template('index.html')
+
 @app.route('/inicio')  # Decorador de la ruta principal
 def inicio():
     return render_template('index.html')
@@ -20,7 +24,7 @@ def login():
 
 @app.route('/Registro') # 
 def Registro():
-    return "render_template('Registro.html')" 
+    return render_template('Registro.html') 
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000) #ejecuta la aplicacion en modo depuracion
