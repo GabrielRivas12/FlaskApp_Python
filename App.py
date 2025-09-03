c4636dbac3d34794f06eb5801220c6ad7d8e511c
GabrielRivas12 <gabrielmrivash@gmail.com>
2025-09-03 14:17:47 -0600

    actualizacion app

--- App.py
+++ App.py
@@ -1,10 +1,47 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+
 
 app=Flask(__name__)
+
+mysql=MySQL() #inicializa la conexion a la DB
+
+# conexion a la DB
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_PORT'] = 3306
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'ventas'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql.init_app(app) #inicializa la conexion a la DB
+
 
 @app.route('/')  # Decorador de la ruta principal
 def home():
     return render_template('index.html')
+
+@app.route('accesologin', methods=['GET', 'POST'])
+def accesologin():
+    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
+        email = request.form['email']
+        password = request.form['password']
+
+        # conexion a la base de datos
+        cursor = mysql.connection.cursor()
+        cursor.execute('SELECT * FROM usuarios WHERE email = %s AND password = %s', (email, password))
+        user = cursor.fetchone()
+        cursor.close()
+
+        if user:
+            session['id_rol'] = user['id_rol']
+            if user['id_rol'] == 1:
+                return render_template('admin.html')
+            elif user['id_rol'] == 2:
+                return render_template('usuario.html')
+        else:
+            return 'Incorrect username/password!'
+        return render_template('Login.html')
 
 @app.route('/inicio')  # Decorador de la ruta principal
 def inicio():
