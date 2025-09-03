96bb7b16856dc23d88cb02963cf16f846343280b
GabrielRivas12 <gabrielmrivash@gmail.com>
2025-09-02 22:47:40 -0600

    Correciones IU de contactos, login, registro y about

--- App.py
+++ App.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app=Flask(__name__)
 
@@ -13,6 +13,20 @@ def inicio():
 @app.route('/contacto')
 def contacto(): # funtion de la ruta de contactos
     return render_template('Contactos.html')
+
+@app.route('/contactopost', methods=['GET', 'POST'])
+def contactopost():
+    usuario={ #Diccionaroio para almacenar los datos del formulario
+         'nombre': '',
+         'email': '',
+         'mensaje': ''
+    }
+    if request.method == 'POST':
+       
+       usuario['nombre'] = request.form.get('nombre')
+       usuario['email'] = request.form.get('email')
+       usuario['mensaje'] = request.form.get('mensaje')
+    return render_template('Contactopost.html', user=usuario)
 
 @app.route('/about') # funtion de la ruta de about
 def about():
